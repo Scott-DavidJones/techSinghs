189f814ee8d38f1ebe3ddcc41e1e88b0b62ad160
Scott-David Jones <scott.jones@autumndev.co.uk>
2020-07-02 12:09:32 +0100

    set default config option for service id
    Signed-off-by: Scott-David Jones <scott.jones@autumndev.co.uk>

--- config/techsinghs.php
+++ config/techsinghs.php
@@ -6,4 +6,5 @@ return [
     'api_key'   => env('TECHSINGHS_API_KEY', ''),
     'timeout'   => (float) env('TECHSINGHS_CLIENT_TIMEOUT', 5.0),
     'enabled'   => (bool) env('TECHSINGHS_ENABLED', false),
+    'serviceId' => (int) env('TECHSINGHS_ENABLED', 14),
 ];
